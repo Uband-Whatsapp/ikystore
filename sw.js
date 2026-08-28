178eb82bb8ade95f59966d2f1fa29116576a6772
Ekk Store <ekkstore.id@gmail.com>
2026-08-28 15:50:44 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// sw.js - RIKY STORE (Kustom)
+// sw.js - RIKY STORE
 const DEFAULT_URL = 'https://iky.store.ekkstore.web.id/';
 const DEFAULT_ICON = 'https://files.catbox.moe/kzg0nc.png';
 const DEFAULT_BADGE = 'https://files.catbox.moe/kzg0nc.png';
